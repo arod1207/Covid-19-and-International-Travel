0b07e22699fcfa71a5ebb53c4267eb9922e9ebe2
Zachary Aguilar <zacharyjag@yahoo.com>
2020-05-31 10:52:07 -0500

    global function for covid api

--- covid19api.js
+++ covid19api.js
@@ -1,25 +1,20 @@
-//variables
-var destination = ""
-
-//event listener for submit button
-$('.submitBtn').on("click",function(e){
-    e.preventDefault();
-
-
-//ajax call for covid summary 
+ //function dozachstuff (countryCode) = $.ajax(url, countryCode)
+function covidCountry(countryCode){
 var settings = {
-    "url": "https://api.covid19api.com/summary",
+    "url": `https://api.covid19api.com/live/country/${countryCode}`,
     "method": "GET",
     "timeout": 0,
-  };
+};
   
   $.ajax(settings).done(function (response) {
     console.log(response);
   });
-  //function dozachstuff (countryCode) = $.ajax(url, countryCode)
+  
+//event listener for submit button
+$('.submitBtn').on("click",function(e){
+    e.preventDefault();
+})
 
+}; 
 
-
-
-
-});
+covidCountry("Mexico")
