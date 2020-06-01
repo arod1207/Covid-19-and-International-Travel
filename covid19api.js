7e27f852367bc71e4826abc93091c68628006c80
Zachary Aguilar <zacharyjag@yahoo.com>
2020-06-01 12:39:15 -0500

    improving li

--- covid19api.js
+++ covid19api.js
@@ -1,13 +1,18 @@
  //function dozachstuff (countryCode) = $.ajax(url, countryCode)
 function covidCountry(CountryCode){
 var settings = {
-    "url": `https://api.covid19api.com/live/country/${CountryCode}`,
+    "url": `https://api.covid19api.com/total/country/${CountryCode}/status/confirmed`,
     "method": "GET",
     "timeout": 0,
   };
   
   $.ajax(settings).done(function (response) {
-    console.log(response);
+    var cases = response[130].Cases
+    console.log(cases);
+//new li to append data to flight details
+var covidLi = $("<li>");
+$("#flight-details").append(covidLi);
+covidLi.text(`Covid Cases: ${cases}`)
   });
   
 //event listener for submit button
@@ -16,8 +21,6 @@ $('.submitBtn').on("click",function(e){
 })
 
 }; 
-covidCountry();
+covidCountry("chile");
 
-//new li to append data to flight details
-var covidLi = $("<li>");
-$("#flight-details").append(covidLi);
+
